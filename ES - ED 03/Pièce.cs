b17a7e2920b52fa4f242fdcfb82a615e9db43328
Sylvain <Villsyl@divtec.ch>
2019-05-02 15:41:31 +0200

    Ajoutez des fichiers projet.

---
+++ ES - ED 03/Pièce.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ES___ED_03
+{
+    class Pièce
+    {
+        protected int posX = 0;
+        protected int posY = 0;
+
+        public Pièce(int posX, int posY)
+        {
+            this.posX = posX;
+            this.posY = posY;
+        }
+    }
+}
